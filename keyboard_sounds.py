94da94999edb90e2b68234cf9596172ea4da19b0
nickbugati <nicholasgongee@aol.com>
2023-10-06 13:13:47 -0400

    more audio files

--- keyboard_sounds.py
+++ keyboard_sounds.py
@@ -12,8 +12,6 @@ SOUNDS = {}
 keys_to_track = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'space', 'enter', 'backspace', 'tab', 'esc', 'ctrl', 'alt', 'shift',
     # ... add any other keys you have sound effects for
 ]
 
